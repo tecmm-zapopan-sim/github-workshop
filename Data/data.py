ae86224b465319e0ee355e97bfc4c4d2ed6ca164
Pedro Ivan Salas <psalas@vansi.com.mx>
2022-04-28 18:24:57 -0500

    Filtrando los datos mayores que 5

--- Data/data.py
+++ Data/data.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 
 def get_data(path):
-    return pd.read_csv(path)
+    return pd.read_csv(path,index_col=0, header=None)
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from Data.data import get_data
 
 if __name__ == '__main__':
     df = get_data("./Data/data.csv")
+    df = df[df.iloc[:,0]>=5]
     print(df.head())
